ef012637c00efdeaa88625894eb6c763068901df
Me <gekoncze@seznam.cz>
2021-11-17 16:22:10 +0100

    added another open method to package browser

--- src/cz/mg/classdetector/PackageBrowser.java
+++ src/cz/mg/classdetector/PackageBrowser.java
@@ -4,8 +4,19 @@ import cz.mg.annotations.classes.Service;
 import cz.mg.annotations.requirement.Mandatory;
 import cz.mg.collections.list.List;
 
+import java.util.StringTokenizer;
+
 
 public @Service class PackageBrowser {
+    public @Mandatory Package open(@Mandatory String path, @Mandatory Package root){
+        List<String> pathParts = new List<>();
+        StringTokenizer tokenizer = new StringTokenizer(path, ".");
+        while(tokenizer.hasMoreTokens()){
+            pathParts.addLast(tokenizer.nextToken());
+        }
+        return open(pathParts, root);
+    }
+
     public @Mandatory Package open(@Mandatory List<String> path, @Mandatory Package root){
         Package currentPackage = root;
         for(String name : path){
